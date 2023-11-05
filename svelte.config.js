02b443d4db4c2199844a7dca4f1d3dfcd62571b6
yaonur <ya_onur@hotmail.com>
2023-11-05 05:45:39 +0300

    ss

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {

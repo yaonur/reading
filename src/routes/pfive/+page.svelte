9370a6d3901b8a0e9303e10c6551dca1ce029848
onur yanık <ya_onur@hotmail.com>
2024-02-13 20:31:40 +0300

    ss

--- src/routes/pfive/+page.svelte
+++ src/routes/pfive/+page.svelte
@@ -13,11 +13,11 @@
 		if (num === oldNum) randomNum();
 	};
 	function handleAnswer(e, answer) {
-        console.log(e.target)
+		console.log(e.target);
 		if (answer === num - 5) {
 			target--;
-            e.target.classList.add('celebrate');
-            setTimeout(() => e.target.classList.remove('celebrate'), 1000);
+			e.target.classList.add('celebrate');
+			setTimeout(() => e.target.classList.remove('celebrate'), 1000);
 			if (target === 0) {
 				isFinished = true;
 				return;
@@ -32,26 +32,27 @@
 	}
 </script>
 
-<div class="flex flex-col justify-center p-32 gap-4">
+<div class="flex flex-col justify-center px-44 py-10 gap-4 text-2xl">
 	{#if isFinished}
 		<p>Tebrikler Bitirdin!!!</p>
 		<button class="h-8 w-20 bg-green-400" on:click={handleRestart}>Yeniden Basla</button>
+	{:else}
+		<p class="text-6xl">{num}=5+x</p>
+		<p class="text-6xl">x=?</p>
+		<div class="flex gap-4">
+			<button class={'bg-blue-300 w-20'} on:click={(e) => handleAnswer(e, 1)}>1</button>
+			<button class={'bg-blue-300 w-20'} on:click={(e) => handleAnswer(e, 2)}>2</button>
+			<button class={'bg-blue-300 w-20'} on:click={(e) => handleAnswer(e, 3)}>3</button>
+			<button class={'bg-blue-300 w-20'} on:click={(e) => handleAnswer(e, 4)}>4</button>
+		</div>
+		<button class="h-8 w-40 bg-green-400" on:click={randomNum}>Siradaki</button>
+		<div>
+			<input type="range" min="10" max="400" bind:value={target} step="10" />
+			<p>Kalan= {target}</p>
+		</div>
+		<!-- Dummy element to prevent Svelte from removing the 'celebrate' class -->
+		<div class="celebrate" style="display: none;"></div>
 	{/if}
-	<p class="text-3xl">{num}=5+x</p>
-	<p class="text-3xl">x=?</p>
-	<div class="flex gap-4">
-		<button class={'bg-blue-300 w-10'} on:click={(e) => handleAnswer(e, 1)}>1</button>
-		<button class={'bg-blue-300 w-10'} on:click={(e) => handleAnswer(e, 2)}>2</button>
-		<button class={'bg-blue-300 w-10'} on:click={(e) => handleAnswer(e, 3)}>3</button>
-		<button class={'bg-blue-300 w-10'} on:click={(e) => handleAnswer(e, 4)}>4</button>
-	</div>
-	<button class="h-8 w-20 bg-green-400" on:click={randomNum}>Siradaki</button>
-	<div>
-		<input type="range" min="10" max="400" bind:value={target} step="10" />
-		<p>Kalan= {target}</p>
-	</div>
-    <!-- Dummy element to prevent Svelte from removing the 'celebrate' class -->
-    <div class="celebrate" style="display: none;"></div>
 </div>
 
 <style>
@@ -59,18 +60,19 @@
 		transform: scale(0.8);
 		transition: transform 0.15s;
 	}
-    .celebrate {
-        animation: celebrate 1.5s;
-    }
+	.celebrate {
+		animation: celebrate 1.5s;
+	}
 
-    @keyframes celebrate {
-        0%, 100% { 
-            transform: scale(1); 
-            background-color: rgb(147 197 253); /* Initial color */
-        }
-        50% { 
-            transform: scale(1.4); 
-            background-color: #4ff13d; /* Color at the midpoint of the animation */
-        }
-    }
+	@keyframes celebrate {
+		0%,
+		100% {
+			transform: scale(1);
+			background-color: rgb(147 197 253); /* Initial color */
+		}
+		50% {
+			transform: scale(1.4);
+			background-color: #4ff13d; /* Color at the midpoint of the animation */
+		}
+	}
 </style>
